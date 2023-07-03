acef48df7321c668a1e593eca8bc94c62bc9591d
Miko Cankudis <mikolaj@cankudis.net>
2023-07-03 02:11:46 +0200

    feat: add indexing data from github

---
+++ config.py
@@ -0,0 +1,10 @@
+from llama_hub.github_repo import GithubRepositoryReader
+owner                   = "nextflow-io"
+repo                    = "nextflow"
+filter_directories      = (["docs"], GithubRepositoryReader.FilterType.INCLUDE)
+# filter_file_extensions  = ([".md", ".groovy", ".java", ".nf"], GithubRepositoryReader.FilterType.INCLUDE)
+filter_file_extensions  = ([".md"], GithubRepositoryReader.FilterType.INCLUDE)
+concurrent_requests     = 10
+branch                  = "master"
+
+persist_dir             = "./nextflow-md-index"
